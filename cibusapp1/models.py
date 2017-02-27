74ab83fe234f3ff6d9df4852bac1461db3b02be4
svahil9911 <vishalchauhan9911@gmail.com>
2017-02-27 23:21:03 +0530

    model.py changed

--- cibusapp1/models.py
+++ cibusapp1/models.py
@@ -20,9 +20,55 @@ class CustomUser(AbstractUser):
 
 class Dish(models.Model):
     name = models.CharField(max_length=200)
-    
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    type_choices = (
+		
+		('NI', 'North Indian'),
+		('SI', 'South Indian'),
+		('FF', 'Fast Food'),
+		('C', 'Continental'),
+		('B', 'Beverage'),
+		('D', 'Desset'),
+		
+	)
+    category = models.CharField(max_length=2,
+								 choices=type_choices,
+								 default='C')
+   
     def __str__(self):
-        return self.choice_text
+        return self.name
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    restaurant = models.ForeignKey(CustomUser)
+    price = models.IntegerField(default=0)
+    
+   
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    orderid = models.IntegerField()
+    type_choices = (
+		
+		('P', 'Under Preparation'),
+		('O', 'Dispatched'),
+		('D', 'Delivered'),
+		
+	)
+    status= models.CharField(max_length=1,
+								 choices=type_choices,
+								 default='C')
+   
+    customer = models.ForeignKey(CustomUser)
+    restaurant = models.ForeignKey(CustomUser)
+    
+   
+    def __str__(self):
+        return self.orderid
+
+class OrderDetails(models.Model):
+	order=models.ForeignKey(Order)
+	dish=models.ForeignKey(Dish)
+	qty=models.IntegerField()
+
 # Create your models here.
