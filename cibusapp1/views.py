ebebbe5f4dbf9824410a4c322a4335a3fa18bf2f
svahil9911 <vishalchauhan9911@gmail.com>
2017-02-28 15:40:52 +0530

    day1 version-2

--- cibusapp1/views.py
+++ cibusapp1/views.py
@@ -144,7 +144,7 @@ def clogin(request):
                 # If the account is valid and active, we can log the user in.
                 # We'll send the user back to the homepage.
                 login(request, user)
-                return HttpResponse("Login success")
+                return redirect(customer)
             else:
                 # An inactive account was used - no logging in!
                 return HttpResponse("Your Rango account is disabled.")
